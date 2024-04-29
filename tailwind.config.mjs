8b2db0f0ed12d8c83bee164a12973010a5c95751
adirav02 <62359835+adirav02@users.noreply.github.com>
2024-04-29 20:07:40 +0300

    Designed Header and navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Prata", "serif"],
+        sans: ["Lato", "sans-serif"],
+      },
+      screens: {
+        "2xl": "1344px",
+        xl: "1200px",
+        lg: "944px",
+        md: "704px",
+        sm: "544px",
+      },
+    },
+  },
+  plugins: [],
+};
