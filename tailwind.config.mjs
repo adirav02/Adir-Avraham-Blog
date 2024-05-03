0273f517c857b9ff773fd8bcb564b9b61bddd865
adirav02 <62359835+adirav02@users.noreply.github.com>
2024-05-03 15:18:37 +0300

    Added 404 Page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,12 +21,15 @@ export default {
         "p-heading": "3.25rem", // equivalent to 52px
         "p-heading-sm": "2.75rem",
         "32px": "2rem",
+        "10xl": "10rem",
       },
       maxHeight: {
         "1/2": "50%",
       },
     },
     letterSpacing: {
+      wider: "0.05em",
+      widest: "0.1em",
       heading: "-0.5px",
       subheading: "0.75px",
     },
